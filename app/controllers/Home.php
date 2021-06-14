2152a6a553643ea369fb851e8542b4be5f056921
svmmerse <71855423+svmmerse@users.noreply.github.com>
2021-06-15 00:13:15 +0700

    tambahan

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,5 +1,5 @@
 <?php
-class home extends Controller
+class Home extends Controller
 {
     public function index()
     {
---
+++ app/controllers/Kategori.php
@@ -0,0 +1,85 @@
+<?php
+
+class Kategori extends Controller
+{
+
+    public function cari()
+    {
+        if (isset($_SESSION['id'])) {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin ';
+            $data['user'] = $this->model('User_model')->getUserByid($_SESSION['id']);
+            $_SESSION['username'] = $data['user']['username'];
+            $_SESSION['foto'] = $data['user']['foto'];
+            $data['hasil_buku'] = $this->model('Barang_model')->cari($_POST);
+            $this->view('templates/header', $data);
+            $this->view('kategori/index', $data);
+            $this->view('templates/footer');
+        } else {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin ';
+            $data['hasil_buku'] = $this->model('Barang_model')->cari($_POST);
+            $this->view('templates/header', $data);
+            $this->view('kategori/index', $data);
+            $this->view('templates/footer');
+        }
+    }
+
+    public function ilmiah()
+    {
+        if (isset($_SESSION['id'])) {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin ';
+            $data['user'] = $this->model('User_model')->getUserByid($_SESSION['id']);
+            $_SESSION['username'] = $data['user']['username'];
+            $_SESSION['foto'] = $data['user']['foto'];
+            $data['hasil_buku'] = $this->model('Barang_model')->getIlmiah();
+            $this->view('templates/header', $data);
+            $this->view('kategori/index', $data);
+            $this->view('templates/footer');
+        } else {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin ';
+            $data['hasil_buku'] = $this->model('Barang_model')->getIlmiah();
+            $this->view('templates/header', $data);
+            $this->view('kategori/index', $data);
+            $this->view('templates/footer');
+        }
+    }
+
+    public function asing()
+    {
+        if (isset($_SESSION['id'])) {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin ';
+            $data['user'] = $this->model('User_model')->getUserByid($_SESSION['id']);
+            $_SESSION['username'] = $data['user']['username'];
+            $_SESSION['foto'] = $data['user']['foto'];
+            $data['hasil_buku'] = $this->model('Barang_model')->getAsing();
+            $this->view('templates/header', $data);
+            $this->view('kategori/index', $data);
+            $this->view('templates/footer');
+        } else {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin ';
+            $data['hasil_buku'] = $this->model('Barang_model')->getgetAsing();
+            $this->view('templates/header', $data);
+            $this->view('kategori/index', $data);
+            $this->view('templates/footer');
+        }
+    }
+
+    public function campuran()
+    {
+        if (isset($_SESSION['id'])) {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin ';
+            $data['user'] = $this->model('User_model')->getUserByid($_SESSION['id']);
+            $_SESSION['username'] = $data['user']['username'];
+            $_SESSION['foto'] = $data['user']['foto'];
+            $data['hasil_buku'] = $this->model('Barang_model')->getCampuran();
+            $this->view('templates/header', $data);
+            $this->view('kategori/index', $data);
+            $this->view('templates/footer');
+        } else {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin ';
+            $data['hasil_buku'] = $this->model('Barang_model')->getCampuran();
+            $this->view('templates/header', $data);
+            $this->view('kategori/index', $data);
+            $this->view('templates/footer');
+        }
+    }
+}
