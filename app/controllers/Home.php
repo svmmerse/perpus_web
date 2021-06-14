f44802540943d9a8f7ba68ffea1f42b7e6d9261f
Mohammad Syafiq <71855423+svmmerse@users.noreply.github.com>
2021-06-14 20:59:20 +0700

    tambahan

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,16 +1,87 @@
-<?php 
-
-class Home extends Controller {
-    private $title = 'Home';
-
-        public function index(){
-
-            if ( !isset($_POST['login']) ) {
-                $data['judul'] = "Perpustakaan UNEJ";
-                $this->view('perpustakaan/login/home',$data);
-            }else {
-                $this->model('Login_models')->login($_POST);
-            }
+<?php
+class home extends Controller
+{
+    public function index()
+    {
+        if (isset($_SESSION['id'])) {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin';
+            $data['user'] = $this->model('User_model')->getUserByid($_SESSION['id']);
+            $_SESSION['username'] = $data['user']['username'];
+            $_SESSION['foto'] = $data['user']['foto'];
+            $this->view('templates/header', $data);
+            $this->view('home/index');
+            $this->view('templates/footer');
+        } else {
+            $data['judul'] = 'Perpustakaan UNEJ | Librarin';
+            $this->view('templates/header', $data);
+            $this->view('home/index');
+            $this->view('templates/footer');
         }
-}
+    }
 
+    public function masuk()
+    {
+        $data['user'] = $this->model('User_model')->login($_POST);
+        if (isset($data['user']['id'])) {
+            $_SESSION['id'] = $data['user']['id'];
+            if ($this->model('User_model')->updatedAt($data['user']['id']) > 0) {
+                header('Location: ' . BASEURL);
+            } else {
+                Flasher::setFlash('Gagal', 'Masuk', 'danger');
+                header('Location: ' . BASEURL . '/home');
+                exit;
+            }
+        } else {
+            Flasher::setFlash('Gagal', 'Masuk', 'danger');
+            header('Location: ' . BASEURL . '/home');
+            exit;
+        }
+    }
+
+    public function pendaftaran() #untuk fungsi pendaftaraan akun baru
+    {
+        $data['judul'] = 'Perpustakaan UNEJ | Librarin';
+        $this->view('templates/header', $data);
+        $this->view('home/pendaftaran');
+        $this->view('templates/footer');
+    }
+
+    public function keluar() #untuk fungsi keluar dari akun
+    {
+        session_destroy();
+        header('Location: ' . BASEURL);
+        exit;
+    }
+
+    public function daftar() #untuk cek berhasil tidaknya dari from akun
+    {
+        if ($_POST['password'] == $_POST['cpassword']) {
+            if ($this->model('User_model')->daftar($_POST, $_FILES) > 0) {
+                Flasher::setFlash('Berhasil', 'Daftar', 'success');
+                header('Location: ' . BASEURL);
+                exit;
+            } else {
+                Flasher::setFlash('Gagal', 'Daftar', 'danger');
+                header('Location: ' . BASEURL);
+                exit;
+            }
+        } else {
+            Flasher::setFlash('Gagal', 'Daftar', 'danger');
+            header('Location: ' . BASEURL);
+            exit;
+        }
+    }
+
+    public function lupa()
+    {
+        if ($this->model('User_model')->lupa($_POST['emaill']) > 0) {
+            Flasher::setFlash('Berhasil', 'Silahkan Cek Email', 'success');
+            header('Location: ' . BASEURL);
+            exit;
+        } else {
+            Flasher::setFlash('Gagal', 'Anda Bukan User', 'danger');
+            header('Location: ' . BASEURL);
+            exit;
+        }
+    }
+}
